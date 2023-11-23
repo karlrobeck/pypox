a6272e61eb10c522e82e09d5b4048c34d541fa5e
karl robeck alferez <karlalferezfx@gmail.com>
2023-11-23 17:24:07 +0800

    fix some issue in setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="pypox",
-    version="0.1.0",
+    version="1.0.0",
     author="Karl Robeck Alferez",
     author_email="karlalferezfx@gmail.com",
-    description="python web framework for building web application using fastapi and react",
+    description="",
     packages=find_packages(),
-    entry_points={"console_scripts": ["pypox=pypox.__init__:app"]},
-    install_requires=["typer", "uvicorn[standard]", "fastapi", "watchfiles", "bs4"],
+    install_requires=["uvicorn[standard]", "fastapi"],
     include_package_data=True,
-    package_data={"": ["*.js", "*.css", "*.html", "*.jsx"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.11.4",
+    python_requires=">=3.11.6",
 )
