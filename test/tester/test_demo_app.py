c86498c685837ff17e17107ef4e7b26d4476aa4f
karl robeck alferez <karlalferezfx@gmail.com>
2023-12-16 16:40:23 +0800

    remove ending dash

--- test/tester/test_demo_app.py
+++ test/tester/test_demo_app.py
@@ -42,7 +42,7 @@ async def test_register():
 
         for key in test_user:
             response = await ac.post(
-                "/auth/register/",
+                "/auth/register",
                 json=test_user[key],
             )
             assert response.status_code == 200 or response.status_code == 409
@@ -53,7 +53,7 @@ async def test_login():
     async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
         for key in test_user:
             response = await ac.post(
-                "/auth/login/",
+                "/auth/login",
                 json={
                     "username": test_user[key]["username"],
                     "password": test_user[key]["password"],
@@ -69,7 +69,7 @@ async def test_postTodo():
         fake_user_gen = faker.Faker()
         for key in test_user:
             response = await ac.post(
-                f"/todo/?user_id={test_user[key]['id']}",
+                f"/todo?user_id={test_user[key]['id']}",
                 json={
                     "title": fake_user_gen.text(max_nb_chars=20),
                     "description": fake_user_gen.paragraph(nb_sentences=3),
@@ -84,7 +84,7 @@ async def test_getTodo():
     async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
         for key in test_user:
             response = await ac.get(
-                f"/todo/?user_id={test_user[key]['id']}",
+                f"/todo?user_id={test_user[key]['id']}",
             )
             test_user[key]["todo"] = response.json()
             assert response.status_code == 200
@@ -96,7 +96,7 @@ async def test_updateTodo():
         for key in test_user:
             for todo in test_user[key]["todo"]:
                 response = await ac.put(
-                    f"/todo/{todo.get('id')}/?user_id={test_user[key]['id']}",
+                    f"/todo/{todo.get('id')}?user_id={test_user[key]['id']}",
                     json={
                         "title": todo.get("title"),
                         "description": todo.get("description"),
@@ -112,7 +112,7 @@ async def test_deleteTodo():
         for key in test_user:
             for todo in test_user[key]["todo"]:
                 response = await ac.delete(
-                    f"/todo/{todo.get('id')}/?user_id={test_user[key]['id']}",
+                    f"/todo/{todo.get('id')}?user_id={test_user[key]['id']}",
                 )
                 assert response.status_code == 200
 
